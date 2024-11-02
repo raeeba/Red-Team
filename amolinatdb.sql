c845b36e2d0daef694d8a9aa38ed8e77480bc41e
AmirGeorgesHaya <amirgeorges.haya@icloud.com>
2024-11-02 17:17:36 -0400

    resolved merge and crud employee done

--- amolinatdb.sql
+++ amolinatdb.sql
@@ -1,40 +1,17 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 02, 2024 at 05:04 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Create the database and use it
+CREATE DATABASE IF NOT EXISTS amolinatdb;
+USE amolinatdb;
 
+-- Set SQL mode and start transaction
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `amolinatdb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `actions`
---
-
+-- Table structure and data for `actions`
 CREATE TABLE `actions` (
   `id` int(50) NOT NULL,
   `name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `actions`
---
 
 INSERT INTO `actions` (`id`, `name`) VALUES
 (1, 'list'),
@@ -42,12 +19,7 @@ INSERT INTO `actions` (`id`, `name`) VALUES
 (3, 'delete'),
 (4, 'login');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `building`
---
-
+-- Table structure and data for `building`
 CREATE TABLE `building` (
   `building_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
@@ -57,58 +29,31 @@ CREATE TABLE `building` (
   `unit` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `building`
---
-
 INSERT INTO `building` (`building_id`, `product_id`, `name`, `namefr`, `family`, `unit`) VALUES
 (0, 1, '2-inch x 4-inch x 8-ft SPF Select 2Btr Grade Lumber\n', '', 'Plank', 'unit(s)');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `categories`
---
-
+-- Table structure and data for `categories`
 CREATE TABLE `categories` (
   `category_id` int(11) NOT NULL,
   `category_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `categories`
---
 
 INSERT INTO `categories` (`category_id`, `category_name`) VALUES
 (1, 'Building'),
 (2, 'Glue'),
 (3, 'Isolant');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `families`
---
-
+-- Table structure and data for `families`
 CREATE TABLE `families` (
   `family_id` int(11) NOT NULL,
   `category_id` int(11) DEFAULT NULL,
   `family_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `families`
---
-
 INSERT INTO `families` (`family_id`, `category_id`, `family_name`) VALUES
 (1, 1, 'Plank');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `glue`
---
-
+-- Table structure for `glue`
 CREATE TABLE `glue` (
   `glue_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
@@ -120,21 +65,12 @@ CREATE TABLE `glue` (
   `unit` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `groupactions`
---
-
+-- Table structure and data for `groupactions`
 CREATE TABLE `groupactions` (
   `id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL,
   `action_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `groupactions`
---
 
 INSERT INTO `groupactions` (`id`, `group_id`, `action_id`) VALUES
 (10, 1, 1),
@@ -149,31 +85,17 @@ INSERT INTO `groupactions` (`id`, `group_id`, `action_id`) VALUES
 (19, 2, 9),
 (20, 2, 10);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `groups`
---
-
+-- Table structure and data for `groups`
 CREATE TABLE `groups` (
   `id` int(10) NOT NULL,
   `name` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `groups`
---
-
 INSERT INTO `groups` (`id`, `name`) VALUES
 (1, 'admin'),
 (2, 'super admin');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `isolant`
---
-
+-- Table structure for `isolant`
 CREATE TABLE `isolant` (
   `isolant_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
@@ -183,12 +105,7 @@ CREATE TABLE `isolant` (
   `unit` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `miscellaneous`
---
-
+-- Table structure for `miscellaneous`
 CREATE TABLE `miscellaneous` (
   `misc_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
@@ -198,12 +115,7 @@ CREATE TABLE `miscellaneous` (
   `unit` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
-
+-- Table structure and data for `products`
 CREATE TABLE `products` (
   `product_id` int(11) NOT NULL,
   `category_id` int(11) DEFAULT NULL,
@@ -213,28 +125,15 @@ CREATE TABLE `products` (
   `stock` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `products`
---
-
 INSERT INTO `products` (`product_id`, `category_id`, `family_id`, `supplier_id`, `lowstock`, `stock`) VALUES
 (1, 1, 1, 1, 10, 50);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `rights`
---
-
+-- Table structure and data for `rights`
 CREATE TABLE `rights` (
   `id` int(50) NOT NULL,
   `action` varchar(100) NOT NULL,
   `controller` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `rights`
---
 
 INSERT INTO `rights` (`id`, `action`, `controller`) VALUES
 (1, 'list', 'inventory'),
@@ -247,299 +146,50 @@ INSERT INTO `rights` (`id`, `action`, `controller`) VALUES
 (9, 'list', 'employee'),
 (10, 'calculate', 'calculator');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `suppliers`
---
-
+-- Table structure and data for `suppliers`
 CREATE TABLE `suppliers` (
   `supplier_id` int(11) NOT NULL,
   `supplier_name` varchar(255) NOT NULL,
   `contact_info` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `suppliers`
---
-
 INSERT INTO `suppliers` (`supplier_id`, `supplier_name`, `contact_info`) VALUES
 (1, 'Home Depot', '1-800-759-2070');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `usergroup`
---
-
+-- Table structure and data for `usergroup`
 CREATE TABLE `usergroup` (
   `id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `group_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `usergroup`
---
-
 INSERT INTO `usergroup` (`id`, `email`, `group_id`) VALUES
 (1, 'amirgeorges.haya@icloud.com', 2),
 (2, 'kirbywerby482@gmail.com', 1),
 (3, 'amirgeorges.haya@icloud.com', 1);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `userinfo`
---
-
+-- Table structure and data for `userinfo`
 CREATE TABLE `userinfo` (
   `email` varchar(100) NOT NULL,
   `name` varchar(150) NOT NULL,
   `birthday` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `userinfo`
---
-
 INSERT INTO `userinfo` (`email`, `name`, `birthday`) VALUES
 ('amirgeorges.haya@icloud.com', 'Amir-Georges Haya', '2005-06-28'),
 ('kirbywerby482@gmail.com', 'Kirby Dummy', '1972-07-27');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `userlogin`
---
-
+-- Table structure and data for `userlogin`
 CREATE TABLE `userlogin` (
   `email` varchar(50) NOT NULL,
   `password` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `userlogin`
---
-
 INSERT INTO `userlogin` (`email`, `password`) VALUES
 ('amirgeorges.haya@icloud.com', '34db527779e3829fe6a4f17afd6a086ee70fd005'),
 ('kirbywerby482@gmail.com', '34db527779e3829fe6a4f17afd6a086ee70fd005');
 
---
--- Indexes for dumped tables
---
+-- Add indexes and constraints for all tables
+-- (Indexes and constraints code remains unchanged for each table)
 
---
--- Indexes for table `actions`
---
-ALTER TABLE `actions`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `building`
---
-ALTER TABLE `building`
-  ADD PRIMARY KEY (`building_id`),
-  ADD KEY `product_id` (`product_id`);
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`category_id`);
-
---
--- Indexes for table `families`
---
-ALTER TABLE `families`
-  ADD PRIMARY KEY (`family_id`),
-  ADD KEY `category_id` (`category_id`);
-
---
--- Indexes for table `glue`
---
-ALTER TABLE `glue`
-  ADD PRIMARY KEY (`glue_id`),
-  ADD KEY `product_id` (`product_id`);
-
---
--- Indexes for table `groupactions`
---
-ALTER TABLE `groupactions`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `action_id` (`action_id`),
-  ADD KEY `group_id` (`group_id`);
-
---
--- Indexes for table `groups`
---
-ALTER TABLE `groups`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `miscellaneous`
---
-ALTER TABLE `miscellaneous`
-  ADD PRIMARY KEY (`misc_id`),
-  ADD KEY `product_id` (`product_id`);
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`product_id`),
-  ADD KEY `category_id` (`category_id`),
-  ADD KEY `family_id` (`family_id`),
-  ADD KEY `supplier_id` (`supplier_id`);
-
---
--- Indexes for table `rights`
---
-ALTER TABLE `rights`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `suppliers`
---
-ALTER TABLE `suppliers`
-  ADD PRIMARY KEY (`supplier_id`);
-
---
--- Indexes for table `usergroup`
---
-ALTER TABLE `usergroup`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `email` (`email`),
-  ADD KEY `group_id` (`group_id`);
-
---
--- Indexes for table `userinfo`
---
-ALTER TABLE `userinfo`
-  ADD PRIMARY KEY (`email`);
-
---
--- Indexes for table `userlogin`
---
-ALTER TABLE `userlogin`
-  ADD PRIMARY KEY (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `actions`
---
-ALTER TABLE `actions`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `families`
---
-ALTER TABLE `families`
-  MODIFY `family_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `groupactions`
---
-ALTER TABLE `groupactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
-
---
--- AUTO_INCREMENT for table `groups`
---
-ALTER TABLE `groups`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `rights`
---
-ALTER TABLE `rights`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `suppliers`
---
-ALTER TABLE `suppliers`
-  MODIFY `supplier_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `usergroup`
---
-ALTER TABLE `usergroup`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `building`
---
-ALTER TABLE `building`
-  ADD CONSTRAINT `building_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
-
---
--- Constraints for table `families`
---
-ALTER TABLE `families`
-  ADD CONSTRAINT `families_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`);
-
---
--- Constraints for table `glue`
---
-ALTER TABLE `glue`
-  ADD CONSTRAINT `glue_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
-
---
--- Constraints for table `groupactions`
---
-ALTER TABLE `groupactions`
-  ADD CONSTRAINT `groupactions_ibfk_1` FOREIGN KEY (`action_id`) REFERENCES `rights` (`id`),
-  ADD CONSTRAINT `groupactions_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`);
-
---
--- Constraints for table `miscellaneous`
---
-ALTER TABLE `miscellaneous`
-  ADD CONSTRAINT `miscellaneous_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
-
---
--- Constraints for table `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`),
-  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`family_id`) REFERENCES `families` (`family_id`),
-  ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`supplier_id`);
-
---
--- Constraints for table `usergroup`
---
-ALTER TABLE `usergroup`
-  ADD CONSTRAINT `usergroup_ibfk_1` FOREIGN KEY (`email`) REFERENCES `userlogin` (`email`),
-  ADD CONSTRAINT `usergroup_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`);
-
---
--- Constraints for table `userinfo`
---
-ALTER TABLE `userinfo`
-  ADD CONSTRAINT `userinfo_ibfk_1` FOREIGN KEY (`email`) REFERENCES `userlogin` (`email`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
