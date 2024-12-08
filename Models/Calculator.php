4efa68ee551536d66852504c92705bf56b649c1c
AmirGeorgesHaya <amirgeorges.haya@icloud.com>
2024-12-08 16:34:14 -0500

    caclulator translation fixed and caluclator itself fixed

--- Models/Calculator.php
+++ Models/Calculator.php
@@ -4,8 +4,12 @@ require_once __DIR__ . '/../Models/Model.php';
 class CalculatorModel extends Model {
     public function calculate($length, $height, $thickness, $spacing, $loadBearing) {
         // Perform calculations
-        $woolNeeded = $length * $height * $thickness; 
-        $planksNeeded = ($length / $spacing) * $loadBearing; 
+        $woolNeeded = $length * $height * ($thickness/12); 
+        
+        $horizontalPlanks = $loadBearing; 
+        $verticalPlanks = ceil($length / ($spacing / 12)); 
+        
+        $planksNeeded = $verticalPlanks + $horizontalPlanks;
     
         return [
             'wool_needed' => $woolNeeded,
